7dcc3dd502c9c2e7195ab6c0152e4f4dca185c31
Coolihor <98967189+Coolihor@users.noreply.github.com>
2022-02-03 18:49:33 +0200

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -3,10 +3,10 @@
 
 # In[ ]:
 
-
 import pandas as pd
 df = pd.DataFrame({'grps': list('aaabbcaabcccbbc'), 'vals': [12,345,3,1,45,14,4,52,54,23,235,21,57,3,87]})
-df.sort_values(by=['grps', 'vals'], inplace = True)
-dd = df[((df['grps']=='a')&(df['vals']>4))|((df['grps']=='b')&(df['vals']>3))|((df['grps']=='c')&(df['vals']>21))]
-dd.groupby(['grps']).agg(sum)
+df.sort_values(by=['grps', 'vals'], ascending = [True,False], inplace = True)
+df.set_index('grps', inplace = True)
+df_concat = pd.concat([df.loc['a'][:3], df.loc['b'][:3],df.loc['c'][:3]])
+df_concat.groupby('grps').agg(sum)
 
